97590da7f129db7254049b5ea5e3a9dc05d879dc
Axel Zwaans <axelzwaans@hotmail.com>
2023-11-29 14:21:18 +0000

    ...

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-import Header from "@/components/header";
-import Footer from "@/components/footer";
-import Hero from "@/components/hero";
-import About from "@/components/about";
-import Featured from "@/components/featured";
-import Recommendation from "@/components/Recommendation";
-import Find from "@/components/Find";
-import Testimonials from "@/components/Testimonials";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Featured from "../components/Featured";
+import About from "../components/About";
+import Recommendation from "../components/Recommendation";
+import Find from "../components/Find";
+import Testimonials from "../components/Testimonials";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
